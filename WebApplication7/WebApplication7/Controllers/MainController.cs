5115f656f2bcd5f92f510bfde05f123e719a83c1
irina-shavrina <87582789+irina-shavrina@users.noreply.github.com>
2024-03-25 23:50:30 +0200

    lr8 update

---
+++ WebApplication7/WebApplication7/Controllers/MainController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text;
+
+public class MainController : Controller
+{
+    [Route("")]
+    public IActionResult Index()
+    {
+        return RedirectToAction("AddProduct", "Main");
+    }
+
+    [HttpGet]
+    [Route("AddProduct")]
+    public IActionResult AddProduct()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("AddProduct")]
+    public IActionResult AddProductPut(string name, string price)
+    {
+        bool result = false;
+        try
+        {
+            Product product = new Product(name, decimal.Parse(price, CultureInfo.InvariantCulture), DateTime.Now);
+            result = ProductRepository.AddProduct(product);
+        }
+        catch { }
+        TempData["Status"] = result;
+        return View("AddProduct");
+    }
+
+    [HttpGet]
+    [HttpPost]
+    [Route("ProductView")]
+    public IActionResult ProductView(string? mode)
+    {
+        ASP.NET.Views.Main.ProductViewModel model;
+        if (mode == null)
+        {
+            model = new ASP.NET.Views.Main.ProductViewModel { Mode = "list", Products = ProductRepository.GetProductList() };
+        }
+ 
+        else
+        {
+            model = new ASP.NET.Views.Main.ProductViewModel { Mode = mode, Products = ProductRepository.GetProductList() };
+        }
+       
+        return View(model);
+    }
+
+}
